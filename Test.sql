e90864f132a612d59e261d6847dfa16839319fd4
Chinmay Khandalkar <chinmay.khandalkar@essec.edu>
2025-09-11 14:53:43 +0100

    Modified the test file

--- Test.sql
+++ Test.sql
@@ -1,1 +1,4 @@
-SELECT * FROM Employees WHERE Department = 'Sales';
+-- Active: 1757598259767@@dbc-0974bb0d-ea44.cloud.databricks.com@3306@samples@accuweather
+
+SELECT * FROM samples.accuweather.forecast_hourly_metric
+LIMIT 10;
